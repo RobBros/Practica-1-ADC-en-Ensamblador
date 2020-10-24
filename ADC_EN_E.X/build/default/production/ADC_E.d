c9098822af84097e8c3f4f06d928459c256c9335
RobBros <71994007+RobBros@users.noreply.github.com>
2020-10-23 21:53:38 -0500

    Add files via upload

---
+++ ADC_EN_E.X/build/default/production/ADC_E.d
@@ -0,0 +1,1 @@
+build/default/production\ADC_E.i: ADC_E.s
---
+++ ADC_EN_E.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=ADC_EN_E.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/ADC_EN_E.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=adcene.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/adcene.x.tar
